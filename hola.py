3a9e4f915454012eb0892d0e8c03dd78611ca0ac
redlapar <rafaeldelaparra@yahoo.com>
2019-04-27 17:55:27 -0400

    Todos los test funcionaron

--- hola.py
+++ hola.py
@@ -2,11 +2,21 @@ def readFile(filename):
     with open(filename) as file:
         return file.readlines()
 
+def getValuesFromSampleLine(line):
+    split = line.split("|")
+    return split[0],split[3]
+
+def getValuesFromSCLLine(line):
+    split = line.split(",")
+    return split[4],split[0],split[3]
+
 def getIdFromSampleLine(line):
-    return line.split("|")[0]
+    # split = line.split("|")
+    return getValuesFromSampleLine(line)[0]
 
 def getIdFromSCLLine(line):
-    return line.split(",")[-1]
+    # split = line.split(",")
+    return getValuesFromSCLLine(line)[0]
 
 def filtrarDosArchivos(sampleFile, sclFile):
     sclLines = readFile(sclFile)
@@ -15,12 +25,14 @@ def filtrarDosArchivos(sampleFile, sclFile):
     outputLines = []
     for sclLine in sclLines:
         for sampleLine in sampleLines:
-            sclID = getIdFromSCLLine(sclLine)
-            sampleID = getIdFromSampleLine(sampleLine)
-            if sclID == sampleID:
-                newline = f"{sclID},{sclLine.split(',')[3]},{sampleLine.split('|')[3]}"
+            sclVal = getValuesFromSCLLine(sclLine)
+            sampleVal = getValuesFromSampleLine(sampleLine)
+            if sclVal[0] == sampleVal[0]:
+                newline = f"{sclVal[0]},{sclVal[1]},{sclVal[2]},{sampleVal[1]}"
                 outputLines.append(newline)
     return outputLines
 
-
-    
+def processFiles(sampleFile, sclFile, outputFile):
+    outputlines = filtrarDosArchivos(sampleFile, sclFile)
+    with open(outputFile, "w") as file:
+        file.writelines(outputlines)
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import hola
+import os
 class MyTest(unittest.TestCase):
     def testReadFileCount(self):
         lines = hola.readFile("sample.csv")
@@ -11,10 +12,23 @@ class MyTest(unittest.TestCase):
 
     def testFiltraSampleDeSCL(self):
         lines = hola.filtrarDosArchivos("sample.csv", "scl.csv")
-        expected = "56997446800,7315290-9,quiero este campo"
+        expected = "56997446800,542,7315290-9,quiero este campo"
         actual = lines[0]
         self.assertEqual(expected, actual)
 
     def testGetIdFromSCLLine(self):
         id = hola.getIdFromSCLLine("9,828,AAA,7103277-8,56989089032")
         self.assertEqual("56989089032", id)
+
+    def testSalidaFile(self):
+        outputfilename = "output.csv"
+        if os.path.isfile(outputfilename):
+            os.remove(outputfilename)
+        hola.processFiles("sample.csv", "scl.csv", outputfilename)
+        self.assertTrue(os.path.isfile(outputfilename))
+        outputlines = hola.readFile(outputfilename)
+
+        expected = "56997446800,542,7315290-9,quiero este campo"
+        actual = outputlines[0]
+        self.assertEqual(expected, actual)
+        
